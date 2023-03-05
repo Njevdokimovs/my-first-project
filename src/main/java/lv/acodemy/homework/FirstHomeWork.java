c7f49fd6420e97f1b27562cd0ec1b299fcb8aeab
Njevdokimovs <nikita.jevdokimovs@gmail.com>
2023-03-05 13:10:53 +0200

    [first_01032023]: byte data type

---
+++ src/main/java/lv/acodemy/homework/FirstHomeWork.java
@@ -0,0 +1,10 @@
+package lv.acodemy.homework;
+
+public class FirstHomeWork {
+    public static void main(String[] args) {
+
+        //byte data type
+        byte myCurrentAge = 29;
+        System.out.println(myCurrentAge);
+    }
+}
